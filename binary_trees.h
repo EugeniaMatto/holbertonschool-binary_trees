7410417e9b29bb92d24a5050feaa2f0acc81fb1b
MatiasMtz <4442@holbertonschool.com>
2022-07-19 18:07:58 +0000

    fix 113

--- binary_trees.h
+++ binary_trees.h
@@ -52,5 +52,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 /* Advanced Tasks */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+bst_t *bst_search(const bst_t *tree, int value);
 
 #endif
