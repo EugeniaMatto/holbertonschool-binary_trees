8d47e8e8b12a6a12684ea2d75b9fb4f89d052654
EugeniaMatto <eugematto26@gmail.com>
2022-07-18 19:15:36 -0700

    Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,26 +1,18 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance_l - height left
+ * binary_tree_balance_h - height
  * @tree: pointer to the root node of the tree
- * Return: height left or if tree is NULL return 0
+ * Return: height or if tree is NULL return 0
  */
-int binary_tree_balance_l(const binary_tree_t *tree)
+int binary_tree_balance_h(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (1 + binary_tree_balance_l(tree->left));
-}
 
-/**
- * binary_tree_balance_r - height right
- * @tree: pointer to the root node of the tree
- * Return: height right or if tree is NULL return 0
- */
-int binary_tree_balance_r(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	return (1 + binary_tree_balance_r(tree->right));
+	if (binary_tree_balance_h(tree->left) > binary_tree_balance_h(tree->right))
+		return (1 + binary_tree_balance_h(tree->left));
+	else
+		return (1 + binary_tree_balance_h(tree->right));
 }
 
 /**
@@ -32,5 +24,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (binary_tree_balance_l(tree) - binary_tree_balance_r(tree));
+	return (binary_tree_balance_h(tree->left) -
+			binary_tree_balance_h(tree->right));
 }
