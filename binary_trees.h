5697e340eabd944b56f8a69ecb0bb044096ff627
MatiasMtz <4442@holbertonschool.com>
2022-07-19 18:01:29 +0000

    task 104

--- binary_trees.h
+++ binary_trees.h
@@ -51,5 +51,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 /* Advanced Tasks */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 
 #endif
