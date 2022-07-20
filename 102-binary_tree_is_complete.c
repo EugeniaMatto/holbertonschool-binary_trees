ca239a3b33dc42cb7cd34e4635110d39f7123b4d
EugeniaMatto <eugematto26@gmail.com>
2022-07-20 16:48:52 -0700

    task 21

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -41,7 +41,7 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 		}
 	}
 	if (tree->left && tree->right)
-		return (binary_tree_is_complete(tree->left) && binary_tree_is_complete(tree->right));
-
+		return (binary_tree_is_complete(tree->left) &&
+				binary_tree_is_complete(tree->right));
 	return (binary_tree_is_complete(tree->left));
 }
