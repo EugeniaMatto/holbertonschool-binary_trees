c5e39ead81e906b5ffb1ef68dddf95e2c67a1dd3
EugeniaMatto <eugematto26@gmail.com>
2022-07-19 21:43:35 -0700

    ex 20

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -10,10 +10,39 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	if (!tree->parent)
 		(func)(tree->n);
+	if (binary_tree_uncle((binary_tree_t *)tree) && tree->parent->left == tree)
+	{
+		binary_tree_levelorder(binary_tree_uncle((binary_tree_t *)tree), func);
+	}	
+
 	if (tree->left)
 		(func)(tree->left->n);
 	if (tree->right)
 		(func)(tree->right->n);
+
+	if (binary_tree_uncle((binary_tree_t *)tree) && tree->parent->left == tree)
+	{
+		binary_tree_levelorder(tree->parent->right, func);
+		
+	}
 	binary_tree_levelorder(tree->left, func);
-	binary_tree_levelorder(tree->right, func);
+}
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ * Return: pointer to the uncle node or null
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *gf;
+
+	if (!node)
+		return (NULL);
+	if (!node->parent || !node->parent->parent)
+		return (NULL);
+	gf = node->parent->parent;
+	if (node->parent == gf->left)
+		return (gf->right);
+	return (gf->left);
 }
