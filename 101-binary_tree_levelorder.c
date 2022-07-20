0adce23cdda4afef8d84d7a0f3065fa512d25048
EugeniaMatto <eugematto26@gmail.com>
2022-07-19 20:51:06 -0700

    task 20

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_levelorder - goes through using level-order traversal
+ * @tree: root of tree
+ * @func: function to print
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || (!tree->left && !tree->right))
+		return;
+	if (!tree->parent)
+		(func)(tree->n);
+	if (tree->left)
+		(func)(tree->left->n);
+	if (tree->right)
+		(func)(tree->right->n);
+	binary_tree_levelorder(tree->left, func);
+	binary_tree_levelorder(tree->right, func);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -57,5 +57,6 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 bst_t *bst_search(const bst_t *tree, int value);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif
