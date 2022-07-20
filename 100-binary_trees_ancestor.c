964eab875fab12b3ccbea0fa312726f389d61825
EugeniaMatto <eugematto26@gmail.com>
2022-07-19 20:40:24 -0700

    betty fix

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -5,7 +5,8 @@
  * @second: 2d node
  * Return: pointer to the ancestor or null
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	binary_tree_t *aux1 = (binary_tree_t *)first, *aux2;
 
